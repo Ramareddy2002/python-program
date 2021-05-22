c3885e5420d8eee49123287c2ab52791b85a3f13
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-22 12:29:55 +0530

    Create Vote.py

---
+++ Vote.py
@@ -0,0 +1,8 @@
+name=raw_input("enter the name")
+age=int(raw_input("enter the age"))
+Print"hello",name
+if age<18:
+    print("sorry")
+    print("u r not eligible for vote")
+else:
+    print("u r eligible for vote")
