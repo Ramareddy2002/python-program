b3d9be82d1961ec8232a860e0ed0175cb0bf1831
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-23 21:52:46 +0530

    Update hackerrank_program.py

--- hackerrank_program.py
+++ hackerrank_program.py
@@ -1,11 +1,11 @@
-n = int(input())
- if (n%2!=0) : 
+n=int(input())
+ if(n%2!=0): 
     print('Weird')
- elif (n>=2 and n<=5):
+ elif(n>=2 and n<=5):
     print('Not Weird')
- elif (n>=6 and n<=20): 
+ elif(n>=6 and n<=20): 
     print('Weird')
- else :
+ else:
     print('Not Weird')
     
 '''
