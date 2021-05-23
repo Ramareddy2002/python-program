99b548dd099ce7aa950e71bff9307444c8bdc231
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-23 11:27:41 +0530

    Update and rename Addition and average of 3 numbers to Addition and average of 3 numbers.py

--- Addition and average of 3 numbers.py
+++ Addition and average of 3 numbers.py
@@ -1,6 +1,6 @@
-a=int(input("enter 1st num:")
-b=int(input("enter 2nd num:")
-c=int(input("enter 3rd num:")
+a=int(input("enter 1st num:"))
+b=int(input("enter 2nd num:"))
+c=int(input("enter 3rd num:"))
 sum=a+b+c 
 avg=(a+b+c)/3 
 print("sum:",sum)
