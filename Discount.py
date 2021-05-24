ee1a52e6933fc9f6ccd63d277610067f4c5ff6fa
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-24 09:58:07 +0530

    Update Discount.py

--- Discount.py
+++ Discount.py
@@ -1,13 +1,13 @@
 
 amount=int(raw_input())
 print "total bill is",amount
-if(amount>=1000):
+if(amount>=1000 and amount<=2000):
   c=(amount*10)/100
   print "Discount on the bill amount is",c
-elif(amount>2000):
+elif(amount>2000 and amount<=3000):
   c=(amount*20)/100
   print "Discount on the bill amount is",c  
-elif(amount>3000):
+elif(amount>3000 and amount<=5000):
   c=(amount*30)/100
   print "Discount on the bill amount is",c
 elif(amount>5000):
