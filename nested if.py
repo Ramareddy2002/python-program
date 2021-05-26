1cb24948f133e3acbc07575d2f99982eb7767c2c
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-26 15:10:19 +0530

    Create nested if.py

---
+++ nested if.py
@@ -0,0 +1,22 @@
+a=int(input("enter a value"))
+if(a<=14):
+  if(a<=9):
+    print("a < 10")
+  else:
+    print("10 >= a <=15")
+else:
+  print("a > 10")
+
+'''
+case 1:-
+5
+a < 10
+
+case 2:-
+13
+10 >= a <=15
+
+case 3:-
+17
+a > 10
+'''
