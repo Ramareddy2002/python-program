2ab61e05de91462bc9ec9776ba1232f7382910be
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-27 19:36:00 +0530

    Update range-fun.py

--- range-fun.py
+++ range-fun.py
@@ -1,8 +1,10 @@
 a=int(input("enter a value"))
 print(range(a))
 print(range(a,10))
+print(range(a,10,2))#syntax range(start,end,step_value)
 '''
 enter a value5
 [0, 1, 2, 3, 4]
 [5, 6, 7, 8, 9]
+[5, 7, 9]
 '''
