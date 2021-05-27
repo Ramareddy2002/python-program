78d9a1813474dd626138bf2203478deecee8d219
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-27 12:22:04 +0530

    Create while_2.py

---
+++ while_2.py
@@ -0,0 +1,20 @@
+i=int(raw_input("enter start value"))
+j=int(raw_input("enter end value"))
+k=int(raw_input("enter step count"))
+while i<=j:
+    print i
+    i+=k
+'''
+output:-
+enter start value5
+enter end value20
+enter step count2
+5
+7
+9
+11
+13
+15
+17
+19
+'''
