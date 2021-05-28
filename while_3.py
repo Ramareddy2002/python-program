211e4e9f18a2735c799adaaba0c36dccab92d6b3
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-28 10:28:42 +0530

    Update while_3.py

--- while_3.py
+++ while_3.py
@@ -20,3 +20,14 @@ enter the  end digit number 5
 95
 number of values ended with 5 in 1 to 100 is 10
 '''
+'''
+n=int(input("enter the  end digit number "))
+i=n
+while(i<=100):
+    if(i%10==n):
+        print(i)
+        i+=1
+    else:
+        i+=1
+print "number of values ended with %d in 1 to 100 is %d" %(n,a)
+'''
