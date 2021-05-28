734c8ff7c147546dd9189697eb52feb98c6d46a0
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-28 09:43:06 +0530

    Update while_2.py

--- while_2.py
+++ while_2.py
@@ -4,12 +4,15 @@ k=int(raw_input("enter step count"))
 while i<=j:
     print i
     i+=k
-    '''
+'''
     Or we can write this program as:-
     i=int(raw_input("enter start value"))
     j=int(raw_input("enter end value"))
     k=int(raw_input("enter step count"))
-    print(range(i,j,k))
+    print(range(i,j,k)) 
+    OUTPUT:-
+    [5, 7, 9, 11, 13, 15, 17, 19]
+'''
 '''
 output:-
 enter start value5
