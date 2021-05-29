af6c00c2aa6560757f4f867e1379f73f4e840285
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-29 20:22:11 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -4,14 +4,14 @@ Count=0
 if(n<2):
     print("wrong number")
 else:
-    while (i<n):
+    while (i<(n//2)+1):
         if(n%i==0):
             Count+=1
         i+=1
     if (Count==1):
         print("%d is a prime number" %n)
     else:
-      print("%d is a prime number" %n)
+      print("%d is not a prime number" %n)
 '''
 enter number:4
 4 is not a prime number
