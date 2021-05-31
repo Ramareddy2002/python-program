caa2bb8a063e3f508d6960054ca0ae731884223f
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-05-31 10:12:58 +0530

    Update for_loop_demo.py

--- for_loop_demo.py
+++ for_loop_demo.py
@@ -1,3 +1,4 @@
+print("1st case")
 end=10
 for i in range(end):
     print(i)
@@ -13,4 +14,18 @@ output:-
 7
 8
 9
+'''
+print("2nd case")
+start=4
+end=11
+for i in range(start,end):
+    print(i)
+'''
+4
+5
+6
+7
+8
+9
+10
 '''
