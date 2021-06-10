9906e9ebc60de0c118f83c88f1fef8996a4afa2a
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-06-10 12:01:05 +0530

    Update functions_in_python.py

--- functions_in_python.py
+++ functions_in_python.py
@@ -3,14 +3,30 @@ def sample(x,y):
     print "b value is ",y
     print "a+b =",
     return x+y
+def sub(x,y):
+    print "a-b =",
+    return x-y
+def sub2(x,y):
+    print "a*b =",
+    return x*y
+def sub3(x,y):
+    print "a/b =",
+    return x/y
 a=int(input())
 b=int(input())
 print sample(a,b)
+print sub(a,b)
+print sub2(a,b)
+print sub3(a,b)
+
+#OUTPUT:-
 '''
-OUTPUT:-
+45
 5
-6
-a value is  5
-b value is  6
-a+b = 11
+a value is  45
+b value is  5
+a+b = 50
+a-b = 40
+a*b = 225
+a/b = 9
 '''
