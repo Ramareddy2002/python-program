eea8d7ddfab34ba5ef1ad1449b338aaa3d3d20bc
Sunil-1508 <84443462+Sunil-1508@users.noreply.github.com>
2021-06-10 11:55:28 +0530

    Create functions_in_python.py

---
+++ functions_in_python.py
@@ -0,0 +1,16 @@
+def sample(x,y):
+    print "a value is ",x 
+    print "b value is ",y
+    print "a+b =",
+    return x+y
+a=int(input())
+b=int(input())
+print sample(a,b)
+'''
+OUTPUT:-
+5
+6
+a value is  5
+b value is  6
+a+b = 11
+'''
